3b374f55e21b8051f07e560682fe0972aae731f1
erased-generic <284558@niuitmo.ru>
2026-05-02 01:13:47 +0300

    skip pm2 iface on assets precompilation

--- config/initializers/start_pm2_iface.rb
+++ config/initializers/start_pm2_iface.rb
@@ -1,3 +1,5 @@
-Thread.new do
-  `NODE_PATH=$(npm root -g) SOCKET_PATH=#{APP_CONFIG['pm2_iface_sock']} node #{APP_CONFIG['pm2_iface']}`
+unless Rake.application.top_level_tasks.include?('assets:precompile')
+  Thread.new do
+    `NODE_PATH=$(npm root -g) SOCKET_PATH=#{APP_CONFIG['pm2_iface_sock']} node #{APP_CONFIG['pm2_iface']}`
+  end
 end
