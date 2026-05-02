a485cbc7987704661ff35a3a0c9cd02a4319d79c
erased-generic <284558@niuitmo.ru>
2026-05-02 21:13:06 +0300

    better detect assets precompilation

--- config/initializers/start_pm2_iface.rb
+++ config/initializers/start_pm2_iface.rb
@@ -1,4 +1,4 @@
-unless Rake.application.top_level_tasks.include?('assets:precompile')
+unless $is_rake
   Thread.new do
     `NODE_PATH=$(npm root -g) SOCKET_PATH=#{APP_CONFIG['pm2_iface_sock']} node #{APP_CONFIG['pm2_iface']}`
   end
